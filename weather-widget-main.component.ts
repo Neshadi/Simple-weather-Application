163b5bb599458dfca580ecd3f576964bae4cb2e7
Neshadi Ranaweera <117301828+Neshadi@users.noreply.github.com>
2022-12-05 16:58:19 +0530

    Update weather-widget-main.component.ts

--- weather-widget-main.component.ts
+++ weather-widget-main.component.ts
@@ -14,24 +14,26 @@ export class WeatherWidgetMainComponent implements OnInit {
     this.WeatherData = {
       main : {},
       isDay: true
-  };
+    };
     this.getWeatherData();
-    console.log(this.WeatherData());
+    console.log(this.WeatherData);
   }
 
   getWeatherData(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q= " + city + " + &appid=" + APIkey')
-    .then(response=>{response.json();}) 
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=ff1bc4683fc7325e9c57e586c20cc03e')
+    .then(response=>response.json())
     .then(data=>{this.setWeatherData(data);})
- // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
- // this.setWeatherData(data);
+
+    // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    // this.setWeatherData(data);
   }
 
-  setWeatherData(data)
+  setWeatherData(data: any){
     this.WeatherData = data;
-    this.WeatherData.temp = (this.WeatherData.main.temp).toFixed(0);
+    this.WeatherData.temp =(this.WeatherData.main.temp).toFixed(0);
     this.WeatherData.pressure = (this.WeatherData.main.pressure).toFixed(0);
     this.WeatherData.humidity = (this.WeatherData.main.humidity).toFixed(0);
+    
+    
   }
-
 }
